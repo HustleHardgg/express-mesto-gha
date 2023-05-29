e4a73240aadeaca7a6991e9f526d23a17a0be712
HustleHard <Yokoback@yandex.ru>
2023-05-29 21:42:01 +0400

    PR 13 ver 1

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const { PORT = 3000, BASE_PATH = 'localhost' } = process.env;
+const { ERROR_NOT_FOUND } = require('./utils/response-status');
+
+const app = express();
+
+const userRouter = require('./routes/users');
+const cardRouter = require('./routes/cards');
+
+// Блок кода для работы с mongoDB
+const mongoDB = 'mongodb://localhost:27017/mestodb';
+mongoose.set('strictQuery', false);
+mongoose.connect(mongoDB);
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  req.user = { _id: '63f7b65a4593a0895f31806e' };
+  next();
+});
+app.use('/cards', cardRouter);
+app.use('/users', userRouter);
+
+app.use('*', (req, res) => {
+  res.status(ERROR_NOT_FOUND).send({ message: 'Запрашиваемая страница не найдена' });
+});
+
+// Служебная информация: адрес запущенного сервера
+app.listen(PORT, () => {
+  console.log(`Адрес сервера — http://${BASE_PATH}:${PORT}`);
+});
---
+++ controllers/users.js
@@ -0,0 +1,92 @@
+/* eslint-disable no-underscore-dangle */
+const mongoose = require('mongoose');
+
+const { ValidationError, CastError } = mongoose.Error;
+
+const User = require('../models/user');
+
+const {
+  ERROR_BAD_REQUEST, ERROR_NOT_FOUND, ERROR_SERVER, SUCCESS_CREATED,
+} = require('../utils/response-status');
+
+// Получение списка пользователей
+const getUserList = (req, res) => {
+  User.find({})
+    .then((userList) => res.send({ data: userList }))
+    .catch((error) => res.status(ERROR_SERVER).send(`На сервере произошла ошибка: ${error}`));
+};
+
+// Получение пользователя по ID
+const getUserId = (req, res) => {
+  User.findById(req.params.userId)
+    .then((selectedUser) => {
+      if (selectedUser) {
+        res.send({ data: selectedUser });
+      } else {
+        res.status(ERROR_NOT_FOUND).send({ message: 'Пользователь по указанному _id не найден' });
+      }
+    })
+    .catch((error) => {
+      // https://mongoosejs.com/docs/api/error.html#error_Error-CastError
+      if (error instanceof CastError) {
+        res.status(ERROR_BAD_REQUEST).send({ message: 'Некорректный _id запрашиваемого пользователя' });
+      } else {
+        res.status(ERROR_SERVER).send(`На сервере произошла ошибка: ${error}`);
+      }
+    });
+};
+
+// Создание пользователя
+const createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar })
+    .then((userObject) => res.status(SUCCESS_CREATED).send({ data: userObject }))
+    .catch((error) => {
+      // https://mongoosejs.com/docs/api/error.html#error_Error-ValidationError
+      if (error instanceof ValidationError) {
+        res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные при создании пользователя' });
+      } else {
+        res.status(ERROR_SERVER).send(`На сервере произошла ошибка: ${error}`);
+      }
+    });
+};
+
+// Обновление профиля пользователя
+const updateUserData = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(req.user._id, { name, about }, {
+    new: true,
+    runValidators: true,
+  })
+    .then((updatedData) => res.send({ data: updatedData }))
+    .catch((error) => {
+      // https://mongoosejs.com/docs/api/error.html#error_Error-CastError
+      if (error instanceof ValidationError) {
+        res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные при обновлении профиля' });
+      } else {
+        res.status(ERROR_SERVER).send(`На сервере произошла ошибка: ${error}`);
+      }
+    });
+};
+
+// Обновление аватара пользователя
+const updateUserAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(req.user._id, { avatar }, {
+    new: true,
+    runValidators: true,
+  })
+    .then((updatedAvatar) => res.send({ data: updatedAvatar }))
+    .catch((error) => {
+      // https://mongoosejs.com/docs/api/error.html#error_Error-CastError
+      if (error instanceof ValidationError) {
+        res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные при обновлении аватара' });
+      } else {
+        res.status(ERROR_SERVER).send(`На сервере произошла ошибка: ${error}`);
+      }
+    });
+};
+
+module.exports = {
+  getUserList, getUserId, createUser, updateUserData, updateUserAvatar,
+};
